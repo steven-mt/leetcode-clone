8ba0f4de389e7e3b7cb1936021240b874d9bba51
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-07-04 18:42:31 +0700

    Add login functionality

--- src/components/Modal/Login.tsx
+++ src/components/Modal/Login.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { authModalState } from "@/atoms/authModalAtom";
 import { useSetRecoilState } from "recoil";
+import { auth } from "@/firebase/firebase";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
 
 type LoginProps = {};
 
@@ -10,8 +13,42 @@ const Login: React.FC<LoginProps> = () => {
     setAuthModalState((prev) => ({ ...prev, type: type }));
   };
 
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const router = useRouter();
+
+  const handleLogin = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputs.email || !inputs.password)
+      return alert("Please fill all fields");
+
+    try {
+      const newUser = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+
+      if (!newUser) return;
+      router.push("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (error) alert(error.message);
+  }, [error]);
+
   return (
-    <form className="space-y-6 px-6 pb-4">
+    <form className="space-y-6 px-6 pb-4" onSubmit={handleLogin}>
       <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
       <div>
         <label
@@ -21,6 +58,7 @@ const Login: React.FC<LoginProps> = () => {
           Your Email
         </label>
         <input
+          onChange={handleInputChange}
           type="email"
           name="email"
           id="email"
@@ -39,6 +77,7 @@ const Login: React.FC<LoginProps> = () => {
           Your Password
         </label>
         <input
+          onChange={handleInputChange}
           type="password"
           name="password"
           id="password"
@@ -54,7 +93,7 @@ const Login: React.FC<LoginProps> = () => {
         className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm
         px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
       >
-        Login
+        {loading ? "Logging in..." : "Log In"}
       </button>
 
       <button
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,30 @@
 import AuthModal from "@/components/Modal/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { authModalState } from "@/atoms/authModalAtom";
 import { useRecoilValue } from "recoil";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
   const authModal = useRecoilValue(authModalState);
+
+  const [user, loading, error] = useAuthState(auth);
+
+  const [pageLoading, setPageLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
+
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
