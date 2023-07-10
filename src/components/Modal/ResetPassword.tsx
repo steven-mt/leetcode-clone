797e52e5660afdba38e24d8437eed2ef260fb273
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-07-10 17:56:19 +0700

    Add reset password functionality

--- src/components/Modal/ResetPassword.tsx
+++ src/components/Modal/ResetPassword.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { auth } from "@/firebase/firebase";
+import React, { useEffect, useState } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
 
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
+  const [email, setEmail] = useState("");
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+
+  const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = await sendPasswordResetEmail(email);
+    if (success) alert("Sent email");
+  };
+
+  useEffect(() => {
+    if (error) alert(error.message);
+  }, [error]);
+
   return (
-    <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+    <form
+      className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8"
+      onSubmit={handleReset}
+    >
       <h3 className="text-xl font-medium  text-white">Reset Password</h3>
       <p className="text-sm text-white ">
         Forgotten your password? Enter your e-mail address below, and we&apos;ll
@@ -21,15 +40,18 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
           type="email"
           name="email"
           id="email"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          onChange={(e) => setEmail(e.target.value)}
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500
+          focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 
+          placeholder-gray-400 text-white"
           placeholder="name@company.com"
         />
       </div>
 
       <button
         type="submit"
-        className={`w-full text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-                bg-brand-orange hover:bg-brand-orange-s `}
+        className={`w-full text-white focus:ring-4 focus:ring-blue-300 font-medium
+        rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s `}
       >
         Reset Password
       </button>
