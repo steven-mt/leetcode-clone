137664eaf1e672086b0b9e66457f48619f8129b4
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-08-23 16:16:27 +0700

    Add confetti

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,5 +1,7 @@
+import useWindowSize from "@/hooks/useWindowSize";
 import { LocalProblem } from "@/utils/types/problem";
-import React from "react";
+import React, { useState } from "react";
+import Confetti from "react-confetti";
 import Split from "react-split";
 import Playground from "./Playground/Playground";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
@@ -7,10 +9,24 @@ import ProblemDescription from "./ProblemDescription/ProblemDescription";
 type WorkspaceProps = { problem: LocalProblem };
 
 const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+  const { width, height } = useWindowSize();
+
+  const [success, setSuccess] = useState(false);
+
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription problem={problem} />
-      <Playground problem={problem} />
+      <div className="bg-dark-fill-2">
+        <Playground problem={problem} />
+        {success && (
+          <Confetti
+            gravity={0.3}
+            tweenDuration={4000}
+            width={width - 1}
+            height={height - 1}
+          />
+        )}
+      </div>
     </Split>
   );
 };
