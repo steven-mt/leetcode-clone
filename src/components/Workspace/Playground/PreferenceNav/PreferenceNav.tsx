e0cad10e6ef2bc57ad90c6cfd1fad8defedd2a46
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-08-24 22:10:50 +0700

    Add fullscreen functionality

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { AiOutlineFullscreen, AiOutlineSetting } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
+import {
+  AiOutlineFullscreen,
+  AiOutlineFullscreenExit,
+  AiOutlineSetting,
+} from "react-icons/ai";
 
 type PreferenceNavProps = {};
 
 const PreferenceNav: React.FC<PreferenceNavProps> = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const handleFullscreen = () => {
+    if (isFullScreen) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+
+    setIsFullScreen(!isFullScreen);
+  };
+
+  useEffect(() => {
+    function exitHandler(e: any) {
+      if (!document.fullscreenElement) {
+        setIsFullScreen(false);
+        return;
+      }
+
+      setIsFullScreen(true);
+    }
+
+    if (document.addEventListener) {
+      document.addEventListener("fullscreenchange", exitHandler);
+      document.addEventListener("webkitfullscreenchange", exitHandler);
+      document.addEventListener("mozfullscreenchange", exitHandler);
+      document.addEventListener("MSFullscreenChange", exitHandler);
+    }
+  });
+
   return (
     <div className="flex items-center justify-between bg-dark-layer-2 h-11 w-full">
       <div className="flex items-center text-white">
@@ -27,11 +61,15 @@ const PreferenceNav: React.FC<PreferenceNavProps> = () => {
           <div className="preferenceBtn-tooltip">Settings</div>
         </button>
 
-        <button className="preferenceBtn group">
+        <button className="preferenceBtn group" onClick={handleFullscreen}>
           <div className="h-4 w-4 text-dark-gray-6 font-bold text-lg">
-            <AiOutlineFullscreen />
+            {isFullScreen ? (
+              <AiOutlineFullscreenExit />
+            ) : (
+              <AiOutlineFullscreen />
+            )}
           </div>
-          <div className="preferenceBtn-tooltip">Full Screen</div>
+          <div className="preferenceBtn-tooltip">Fullscreen</div>
         </button>
       </div>
     </div>
